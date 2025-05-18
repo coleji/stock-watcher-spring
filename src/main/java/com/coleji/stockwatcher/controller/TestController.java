15b2e129ca291fa53c5ae7a6e269eaa390c7dc14
Jonathan Cole <jon@jcole.io>
2025-05-18 18:55:22 -0400

    ohlc model

--- src/main/java/com/coleji/stockwatcher/controller/TestController.java
+++ src/main/java/com/coleji/stockwatcher/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.coleji.stockwatcher.controller;
 
 import com.coleji.stockwatcher.config.prop.AppProps;
+import com.coleji.stockwatcher.model.polygon.DtoOhlcResponse;
+import com.coleji.stockwatcher.util.JacksonUtil;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestController("/")
 class TestController {
@@ -27,6 +36,23 @@ class TestController {
 
 	@GetMapping("/")
 	public String ping() {
+		List<File> allFiles = Stream.of(new File("out/sample/ohlc").listFiles())
+			.filter(file -> !file.isDirectory())
+			.sorted(Comparator.comparing(File::getName))
+			.toList();
+		for (File f : allFiles) {
+			try (FileInputStream fis = new FileInputStream(f)) {
+				byte[] contents = fis.readAllBytes();
+				ObjectMapper mapper = JacksonUtil.getObjectMapperBuilderWithModules()
+						.enable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+						.build();
+				DtoOhlcResponse dto = mapper.readValue(contents, DtoOhlcResponse.class);
+				logger.info(f.getName() + " - " + dto.getResultsCount());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
 		return "pong";
 	}
 }
